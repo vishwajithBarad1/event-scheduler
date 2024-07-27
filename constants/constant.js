e528991f1d5302881c56b79c642bb9c28a7c7ff6
vishwajithBarad1 <vishwajithbarad@gmail.com>
2024-07-28 03:23:10 +0530

    masked the mongooseUrl

--- constants/constant.js
+++ constants/constant.js
@@ -1,4 +1,4 @@
 const PORT = 3000;
-const mongooseUrl = "mongodb+srv://vishwajithbarad:q9tHEkXOTkQW8GPZ@cluster0.jmxn696.mongodb.net/eventScheduler";
+const mongooseUrl = "mongodb+srv://vishwajithbarad:****************@cluster0.jmxn696.mongodb.net/eventScheduler";
 
 module.exports = {PORT,mongooseUrl};
